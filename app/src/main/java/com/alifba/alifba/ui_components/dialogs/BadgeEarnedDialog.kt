09cb1d3e8e58316a76abacf6152af7d0abde3daf
rimshad <rimshadpcs@gmail.com>
2025-01-13 14:44:18 +0000

    profile section changes, xp,streak, etc,and badges

--- app/src/main/java/com/alifba/alifba/ui_components/dialogs/BadgeEarnedDialog.kt
+++ app/src/main/java/com/alifba/alifba/ui_components/dialogs/BadgeEarnedDialog.kt
@@ -1,15 +1,12 @@
 package com.alifba.alifba.ui_components.dialogs
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Shapes
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -20,14 +17,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
 import com.alifba.alifba.R
 import com.alifba.alifba.data.models.Badge
 import com.alifba.alifba.ui_components.theme.lightNavyBlue
